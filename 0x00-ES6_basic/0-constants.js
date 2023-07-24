ee0af9ca6ecf3b96fbf0258d85993c4083c16c71
Elvis rozy <elvisrozy52@gmail.com>
2023-07-24 15:11:09 +0100

    task 0 second attempt

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -8,8 +8,7 @@ export function getLast() {
 }
 
 export function taskNext() {
-	let combination = 'But sometimes ';
-	combination+= 'using just let';
+	let combination = 'But sometimes let';
 	combination += getLast();
 
 	return combination;
