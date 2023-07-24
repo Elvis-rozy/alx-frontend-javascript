37f40a13b7cc9cc7772920935856dc7cb31bdbe4
Elvis rozy <elvisrozy52@gmail.com>
2023-07-24 17:59:39 +0100

    trying new fixes

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -3,5 +3,5 @@ export default function iterateThroughObject(reportWithIterator) {
 	for (const item of reportWithIterator) {
 		employees += `${item} | `;
 	}
-	return employees.slice(0, -3);
+	return employees.slice(0, employees.length-3);
 }
